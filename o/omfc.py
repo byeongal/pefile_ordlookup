954428e7d67fa065636721f85ba9567c4516273c
김영재 <corea_kyj@naver.com>
2020-01-06 17:32:30 +0900

    omfc.dll (39adedbc5908e9bc878669fc52078dcb)

---
+++ o/omfc.py
@@ -0,0 +1,13 @@
+
+ord_names = {
+    1630: b'_ctime64',
+    6703: b'_gmtime64',
+    6704: b'_localtime64',
+    6705: b'_mktime64',
+    6706: b'_resetstkoflw',
+    6707: b'_scprintf',
+    6708: b'_scwprintf',
+    6709: b'_time64',
+    6710: b'_vscprintf',
+    6711: b'_vscwprintf',
+}
