b299df3eecebed6f31e370f27bed4d86b874ca57
김영재 <corea_kyj@naver.com>
2020-01-06 15:42:10 +0900

    mfc42u.dll (5550eb68e60a0efcafa296c8b2ac9303)

---
+++ m/mfc42u.py
@@ -0,0 +1,11 @@
+
+ord_names = {
+    7: b'DllCanUnloadNow',
+    8: b'DllGetClassObject',
+    9: b'DllRegisterServer',
+    10: b'DllUnregisterServer',
+    1446: b'?AfxFreeLibrary@@YAHPEAUHINSTANCE__@@@Z',
+    1487: b'?AfxLoadLibrary@@YAPEAUHINSTANCE__@@PEBG@Z',
+    1490: b'?AfxLockGlobals@@YAXH@Z',
+    1576: b'?AfxUnlockGlobals@@YAXH@Z',
+}
