c1a44a8299a54debdd8e8197c51c542132fd3839
김영재 <corea_kyj@naver.com>
2020-01-06 15:47:27 +0900

    mfc42.dll (7e4d1b552ee1dfa859ba9033b3670590)

---
+++ m/mfc42.py
@@ -0,0 +1,9 @@
+
+ord_names = {
+    1: b'DllGetClassObject',
+    2: b'DllCanUnloadNow',
+    3: b'DllRegisterServer',
+    4: b'DllUnregisterServer',
+    5: b'?classCCachedDataPathProperty@CCachedDataPathProperty@@2UCRuntimeClass@@B',
+    6: b'?classCDataPathProperty@CDataPathProperty@@2UCRuntimeClass@@B',
+}
