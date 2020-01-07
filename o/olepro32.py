15399017c8ab3b216f1df9b49b734107999d1a0a
김영재 <corea_kyj@naver.com>
2020-01-07 12:32:20 +0900

    olepro32.dll (29cbdb71b0558448282df4aaeb79105d)

---
+++ o/olepro32.py
@@ -0,0 +1,14 @@
+
+ord_names = {
+    248: b'OleIconToCursor',
+    249: b'OleCreatePropertyFrameIndirect',
+    250: b'OleCreatePropertyFrame',
+    251: b'OleLoadPicture',
+    252: b'OleCreatePictureIndirect',
+    253: b'OleCreateFontIndirect',
+    254: b'OleTranslateColor',
+    255: b'DllCanUnloadNow',
+    256: b'DllGetClassObject',
+    257: b'DllRegisterServer',
+    258: b'DllUnregisterServer',
+}
